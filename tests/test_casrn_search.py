3519fed14b8e9d567ed9a7a7044688baf5483e75
Caleb Grant <geocoug@gmail.com>
2023-01-05 05:00:44 -0800

    more tests
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- tests/test_casrn_search.py
+++ tests/test_casrn_search.py
@@ -4,7 +4,14 @@ from typing import Generator
 import pytest
 import requests
 
-from casrn_search import casrn_search, read_csv, send_request, write_csv
+from casrn_search import (
+    casrn_search,
+    clparser,
+    read_csv,
+    send_request,
+    validate_args,
+    write_csv,
+)
 
 TEST_INPUT_FILE = "chemicals.csv"
 TEST_OUTPUT_FILE = "srs_chemicals.csv"
@@ -13,9 +20,35 @@ TEST_OUTPUT_FILE = "srs_chemicals.csv"
 @pytest.fixture(autouse=True)
 def cleanup():
     """Fixture to execute asserts before and after a test is run."""
+    # Before test
     yield
+    # After test
     if os.path.exists(TEST_OUTPUT_FILE):
         os.remove(TEST_OUTPUT_FILE)
+
+
+def test_clparser():
+    parser = clparser()
+    args = parser.parse_args(["-v", "-s", "foo.csv", "bar.csv"])
+    assert args.synonyms
+    assert args.input_file == "foo.csv"
+    args = parser.parse_args(["-s", "foo.csv", "bar.csv"])
+    assert not args.verbose
+    with pytest.raises(SystemExit):
+        args = parser.parse_args(["bar.csv"])
+
+
+def test_validate_args():
+    parser = clparser()
+    args = parser.parse_args(["foo.csv", "bar.csv"])
+    with pytest.raises(FileNotFoundError):
+        validate_args(args)
+    args = parser.parse_args(["requirements.txt", "bar"])
+    with pytest.raises(Exception):
+        validate_args(args)
+    args = parser.parse_args(["chemicals.csv", "bar"])
+    with pytest.raises(Exception):
+        validate_args(args)
 
 
 def test_read_csv():
@@ -58,6 +91,9 @@ def test_casrn_search_with_synonyms():
     casrn_search(TEST_INPUT_FILE, TEST_OUTPUT_FILE, synonyms=True)
     rows = read_csv(TEST_OUTPUT_FILE)
     assert next(rows)[-1] == "synonyms"
+    next(rows)
+    next(rows)
+    assert len(next(rows)[-1]) > 0
 
 
 def test_all_codes_searched():
